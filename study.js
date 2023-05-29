1d984367531ef2f4dea54b2e46624ddd0d191d14
hoseon07 <a01081930429@gmail.com>
2023-05-30 08:33:05 +0900

    Upload

---
+++ study.js
@@ -0,0 +1,11 @@
+let sum = (a, b) => a + b;
+console.log(sum(1, 2));
+
+let sub = (a, b) => a - b;
+console.log(sub(2, 1));
+
+let mul = (a, b) => a * b;
+console.log(mul(2, 3));
+
+let div = (a, b) => a / b;
+console.log(div(4, 2));
